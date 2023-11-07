f17d5294397b071034b984909ddc1acf04ca45f9
Md-ShakilHossain <shakilh550@gmail.com>
2023-11-07 14:35:13 +0600

    added an api for one job with job/:id

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -31,7 +31,14 @@ async function run() {
         const cursor = allJobsCollection.find();
         const result = await cursor.toArray();
         res.send(result);
-    })
+    });
+
+    app.get('/alljobs/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const job = await allJobsCollection.findOne(query);
+      res.send(job);
+    });
 
 
 
